b2a18036f5711dfd914013fdbcded8e1c374f72f
Ravi2320 <rbkadam2320@gmail.com>
2024-02-13 20:52:29 +0530

    Clone an Undirected Graph

---
+++ 35. Clone an Undirected Graph.cpp
@@ -0,0 +1,219 @@
+/*
+Clone an Undirected Graph
+MediumAccuracy: 67.49%Submissions: 19K+Points: 4
+Internship Alert!
+Become an SDE Intern by topping this monthly leaderboard! 
+
+banner
+Given a connected undirected graph with n nodes and m edges, with each node having a distinct label from 0 to n-1, create a clone of the graph. Each node in the graph contains an integer val and an array (neighbors) of nodes, containing nodes that are adjacent to the current node.
+
+Note: If the user returns a correct copy of the given graph, then the system will print 1; in the case when an incorrect copy is generated or when the user returns the original node, the system will print 0.
+
+For Example :    
+
+class Node {
+    val: integer
+    neighbors: List[Node]
+}
+Example 1:
+
+Input:
+adjList = 
+{
+    {1, 3},
+    {0, 2},
+    {1, 3},
+    {0, 2}
+}
+Output: 1
+Explanation:
+1 is the output that the driver code will print in case 
+you successfully cloned the given graph.
+Example 2:
+
+Input:
+adjList = 
+{
+    {1},
+    {0}
+}
+Output: 1
+Explanation: 
+1 is the output that the driver code will print in case
+you successfully cloned the given graph.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function cloneGraph( ) which takes a node will always be the first node of the graph as input and returns the copy of the given node as a reference to the cloned graph.
+
+Expected Time Complexity: O(n + m).
+Expected Auxiliary Space: O(n).
+
+Constraints:
+1 <= n, m <= 104
+1 <= Node value <= n
+*/
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+#include <sstream>
+using namespace std;
+
+struct Node {
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+vector<Node*> bfs(Node *src){
+    vector<Node*>ans;
+    map<Node*, bool> visit;
+    queue<Node*> q;
+    q.push(src);
+    visit[src] = true;
+    while (!q.empty()) {
+        Node *u = q.front();
+        ans.push_back(u);
+        q.pop();
+        vector<Node *> v = u->neighbors;
+        int n = v.size();
+        for (int i = 0; i < n; i++){
+            if (!visit[v[i]]){
+                visit[v[i]] = true;
+                q.push(v[i]);
+            }
+        }
+    }
+    return ans;
+}
+
+bool compare(Node* prev, Node* new_node, unordered_set<Node*>& prev_vis, unordered_set<Node*>& new_vis) {
+    if (prev == new_node) {
+        return false;
+    }
+    if (!prev || !new_node) {
+        if ((!prev && new_node) || (prev && !new_node)) {
+            return false;
+        }
+        return true;
+    }
+
+    if (prev_vis.count(prev) || new_vis.count(new_node)) {
+        if ((prev_vis.count(prev) && !new_vis.count(new_node)) || (!prev_vis.count(prev) && new_vis.count(new_node))) {
+            return false;
+        }
+        return true;
+    }
+    prev_vis.insert(prev);
+    new_vis.insert(new_node);
+
+    if (prev->val != new_node->val) {
+        return false;
+    }
+
+    size_t prev_n = prev->neighbors.size();
+    size_t new_n = new_node->neighbors.size();
+    if (prev_n != new_n) {
+        return false;
+    }
+
+    sort(prev->neighbors.begin(), prev->neighbors.end(), [](Node* a, Node* b) { return a->val < b->val; });
+    sort(new_node->neighbors.begin(), new_node->neighbors.end(), [](Node* a, Node* b) { return a->val < b->val; });
+
+    for (size_t i = 0; i < prev_n; ++i) {
+        if (!compare(prev->neighbors[i], new_node->neighbors[i], prev_vis, new_vis)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+// struct Node {
+//     int val;
+//     vector<Node*> neighbors;
+//     Node() {
+//         val = 0;
+//         neighbors = vector<Node*>();
+//     }
+//     Node(int _val) {
+//         val = _val;
+//         neighbors = vector<Node*>();
+//     }
+//     Node(int _val, vector<Node*> _neighbors) {
+//         val = _val;
+//         neighbors = _neighbors;
+//     }
+// };
+
+class Solution {
+public:
+    void dfs(Node* node, Node *ans, unordered_map<int, Node*> &vis){
+        vis[node->val] = ans;
+        for(auto x : node->neighbors){
+            if(vis.find(x->val) == vis.end()){
+                Node* newNode = new Node(x->val);
+                ans->neighbors.push_back(newNode);
+                dfs(x, newNode, vis);
+            }
+            else{
+                ans->neighbors.push_back(vis[x->val]);
+            }
+        }
+    }
+    Node* cloneGraph(Node* node) {
+        Node* ans = new Node(node->val);
+        unordered_map<int, Node*> vis;
+        dfs(node, ans, vis);
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Node* root = NULL;
+        vector<Node*>v(N);
+        std::string buffer;
+        std::getline(std::cin, buffer);
+        for (int i = 0; i < N; i++)v[i] = new Node(i);
+        for (int i = 0; i < N; i++) {
+            std::vector<Node*> vec;
+            std::string buffer;
+            int data;
+            std::getline(std::cin, buffer);
+            std::istringstream iss(buffer);
+            while (iss >> data)
+                vec.push_back(v[data]);
+            v[i]->neighbors = vec;
+        }
+        Solution ob;
+        vector<Node*>prev = bfs(v[0]);
+        Node* ans = ob.cloneGraph(v[0]);
+        //vector<Node*>now = bfs(ans);
+        unordered_set<Node*>prev_vis, new_vis;
+        cout << compare(v[0], ans, prev_vis, new_vis) << endl;
+
+    }
+    return 0;
+}
+// } Driver Code Ends
