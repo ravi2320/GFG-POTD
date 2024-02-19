264c19aac2ccbece31d7ece94ada422d1d1f8ef1
Ravi2320 <rbkadam2320@gmail.com>
2024-02-19 23:14:01 +0530

    Game with String

---
+++ 37. Game with String.cpp
@@ -0,0 +1,60 @@
+/*
+Game with String
+MediumAccuracy: 53.96%Submissions: 46K+Points: 4
+Internship Alert!
+Become an SDE Intern by topping this monthly leaderboard! 
+
+banner
+Given a string s of lowercase alphabets and a number k, the task is to print the minimum value of the string after removal of k characters. The value of a string is defined as the sum of squares of the count of each distinct character present in the string. 
+
+Example 1:
+
+Input: 
+s = abccc, k = 1
+Output: 
+6
+Explaination:
+We remove c to get the value as 12 + 12 + 22
+Example 2:
+
+Input: 
+s = aabcbcbcabcc, k = 3
+Output: 
+27
+Explaination: 
+We remove two 'c' and one 'b'. Now we get the value as 32 + 32 + 32.
+Your Task:
+You do not need to read input or print anything. Your task is to complete the function minValue() which takes s and k as input parameters and returns the minimum possible required value.
+
+Expected Time Complexity: O(n+klog(p))  where n is the length of string and p is number of distinct alphabets and k number of alphabets to be removed. 
+Expected Auxiliary Space: O(n)
+
+Constraints:
+0 ≤ k ≤ |string length| ≤ 105
+*/
+
+class Solution{
+public:
+    int minValue(string s, int k){
+        unordered_map<char, int> mp;
+        for(char ch : s)
+            mp[ch]++;
+        
+        priority_queue<int> pq;
+        for(auto x : mp)
+            pq.push(x.second);
+        while(k--){
+            int temp = pq.top();
+            pq.pop();
+            temp--;
+            pq.push(temp);
+        }
+        int ans = 0;
+        while(pq.size()){
+            int temp = pq.top();
+            pq.pop();
+            ans += temp * temp;
+        }
+        return ans;
+    }
+};
